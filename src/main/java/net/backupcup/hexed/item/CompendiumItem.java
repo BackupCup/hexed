8ce78e1256b5d906dbc36e42e428ae89a322624c
ZeusIGN <ZeusCovert@gmail.com>
2024-12-09 23:11:44 +0200

    mmmm particle

--- src/main/java/net/backupcup/hexed/item/CompendiumItem.java
+++ src/main/java/net/backupcup/hexed/item/CompendiumItem.java
@@ -6,6 +6,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import oshi.util.tuples.Pair;
 import team.lodestar.lodestone.handlers.screenparticle.ParticleEmitterHandler;
+import team.lodestar.lodestone.handlers.screenparticle.ScreenParticleHandler;
+import team.lodestar.lodestone.registry.common.particle.LodestoneScreenParticleTypes;
 import team.lodestar.lodestone.systems.easing.Easing;
 import team.lodestar.lodestone.systems.particle.builder.ScreenParticleBuilder;
 import team.lodestar.lodestone.systems.particle.data.GenericParticleData;
@@ -23,21 +25,21 @@ public class CompendiumItem extends Item implements ParticleEmitterHandler.ItemP
 
     @Override
     public void spawnLateParticles(ScreenParticleHolder target, Level level, float partialTick, ItemStack stack, float x, float y) {
-        float gameTime  = level.getGameTime();
+        float gameTime = level.getGameTime();
         Pair<Color, Color> colorPair = new Pair<>(new Color(0x8A1B29), new Color(0xDC001E));
 
-        SpinParticleDataBuilder spinDataBuilder =
-                SpinParticleData.create(0f, 1f).setSpinOffset(0.025f * gameTime % 6.28f).setEasing(Easing.EXPO_IN_OUT);
+        SpinParticleDataBuilder spinDataBuilder = SpinParticleData.create(0f, 1f).setSpinOffset(0.025f * gameTime % 6.28f).setEasing(Easing.EXPO_IN_OUT);
 
-        ScreenParticleBuilder.create(RegisterParticles.FIRE_RUNE_SCREEN, target)
-                .setTransparencyData(GenericParticleData.create(1f, 0f).setEasing(Easing.QUINTIC_IN).build())
-                .setLifetime(7)
-                .setRandomOffset(0.05)
-
-                .setScaleData(GenericParticleData.create((float) (Math.sin((gameTime * 0.05f)) * 1f), 0.25f).build())
-
-                .setColorData(ColorParticleData.create(colorPair.getA(), colorPair.getA()).build())
-                .setSpinData(spinDataBuilder.setSpinOffset(0.785f - 0.01f * gameTime % 6.28f).build())
-                .spawnOnStack(-0.0, -0.0);
+        for (int i = 0; i < 2; i++) {
+            int xOffset = (int) (level.random.nextFloat() * 32 - 16);
+            int yOffset = (int) (level.random.nextFloat() * 32 - 16);
+            ScreenParticleBuilder.create(RegisterParticles.FIRE_RUNE_SCREEN, target)
+                    .setLifetime(16)
+                    .setColorData(ColorParticleData.create(colorPair.getA(), colorPair.getA()).build())
+                    .setSpinData(spinDataBuilder.setSpinOffset(0.785f - 0.01f * gameTime % 6.28f).build())
+                    .setScaleData(GenericParticleData.create(0.3F).build())
+                    .addMotion(((ScreenParticleHandler.currentItemX - (double) xOffset / 2) - ScreenParticleHandler.currentItemX) / 10, ((ScreenParticleHandler.currentItemY + (double) yOffset / 2) - ScreenParticleHandler.currentItemY) / 10)
+                    .spawnOnStack(xOffset, -yOffset);
+        }
     }
 }
